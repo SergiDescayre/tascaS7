58a7de68935208ef6b588c20a7375cdd3a1badef
SergiDescayre <lepetitdesca@gmail.com>
2024-03-12 18:01:34 +0100

    url dinàmica

--- tascaS7/src/features/starships/starShipThunk.js
+++ tascaS7/src/features/starships/starShipThunk.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const URL = "https://swapi.py4e.com/api/starships"
+const url = "https://swapi.py4e.com/api/starships"
 
 export const fetchStarships = createAsyncThunk(
-    'starShips/fetchStarships',async () => {
+    'starShips/fetchStarships',async (url) => {
         try {
-            const response =  await fetch(URL)
+            const response =  await fetch(url)
             const data = await response.json()
             return data
         } catch (error) {
--- tascaS7/src/pages/StarShips.jsx
+++ tascaS7/src/pages/StarShips.jsx
@@ -1,13 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
+
 import { fetchStarships } from "../features/starships/starShipThunk";
-import { useEffect } from "react";
+import { useEffect ,useState } from "react";
 import StarShipsCard from "../components/starShipsCard";
 
 const StarShips = () => {
+
+    const [url,setUrl] = useState("https://swapi.py4e.com/api/starships")
+
     const dispatch = useDispatch()
     const {isloading , data} = useSelector((state => state.starShips))
     useEffect(() => {
-        dispatch(fetchStarships())
+        dispatch(fetchStarships(url))
+        console.log(data)
     }, [])   
     if(isloading){
         return (
