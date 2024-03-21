3618f9f196d6fcac24c5cc1f87af7f4ad9cdea74
Sergi Descayre <lepetitdesca@gmail.com>
2024-03-21 12:52:19 +0100

    maquetació pàgina no trobada

--- src/components/ImageNotFound.jsx
+++ src/components/ImageNotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import starNotFound from "../assets/star-wars-not.jpg"
 
 const ImageNotFound = ({title}) => {
   return (
-    <div>
-      Image {title } Not Found
+    <div className="container_img_not rounded-lg mt-3 flex items-center justify-center">
+      <div className="bg-slate-950 bg-opacity-75 w-[100%] text-center py-6">
+        <span className="text-red-500 font-bold text-2xl uppercase">Image { title } Not Found</span>
+      </div>
     </div>
   )
 }
